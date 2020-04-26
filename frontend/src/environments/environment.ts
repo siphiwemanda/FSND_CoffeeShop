d612baf959380e5b0c7f0d313f7cc9839a400a7f
siphiwe <siphiwemanda@gmail.com>
2020-04-26 14:28:49 +0100

    standing up front end

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'coffee-shop-fsnd-siphiwemanda.eu', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: 'GXLsZhRZiO8KkrXtL2apZ9i0Vyqjpig9', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:4200/tabs/drink-menu', // the base url of the running ionic application.
   }
 };
